0631198af6158ea77ea97b5542daf62cf1c56904
JoMI103 <a21078@alunos.ipca.pt>
2022-01-15 17:06:09 +0000

    PlayerFragment createViewUpdate again

--- app/src/main/java/com/example/projetocm/PlayerFragment.kt
+++ app/src/main/java/com/example/projetocm/PlayerFragment.kt
@@ -5,10 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.projetocm.databinding.FragmentPlayerBinding
 
 
 class PlayerFragment : Fragment() {
 
+    private var _binding: FragmentPlayerBinding? = null
+    private val binding get() = _binding!!
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+
+
+        _binding = FragmentPlayerBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        return root
+
+
+    }
 
 }
