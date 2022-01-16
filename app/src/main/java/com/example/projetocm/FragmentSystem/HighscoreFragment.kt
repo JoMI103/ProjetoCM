5210ad4d3f270877c8f9ec4fd3446997ac615665
JoMI103 <a21078@alunos.ipca.pt>
2022-01-16 23:33:31 +0000

    Finally Fu adapter

--- app/src/main/java/com/example/projetocm/FragmentSystem/HighscoreFragment.kt
+++ app/src/main/java/com/example/projetocm/FragmentSystem/HighscoreFragment.kt
@@ -1,36 +1,134 @@
 package com.example.projetocm.FragmentSystem
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ListView
 import android.widget.TextView
+import androidx.fragment.app.Fragment
 import com.example.projetocm.GameSystem.Scores
 import com.example.projetocm.R
 import com.example.projetocm.databinding.FragmentHighscoreBinding
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import kotlin.collections.ArrayList
+import com.google.firebase.firestore.DocumentSnapshot
+
+import com.google.firebase.firestore.QuerySnapshot
+
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.ktx.toObject
+import kotlin.math.log
+
 
 class  HighscoreFragment : Fragment() {
 
     private var _binding: FragmentHighscoreBinding? = null
     private val binding get() = _binding!!
-    private val scores = ArrayList<Int>()
+
     lateinit var listviewScores: ListView
-    private val Scores = ArrayList<Int>()
-    lateinit var listviewPositions: ListView
+    private  var scoresList = mutableListOf<Scores>()
+
+    //private val Scoress = ArrayList<Int>()
+    //lateinit var listviewPositions: ListView
+    //private val scores = ArrayList<Int>()
+
+    val adapter = PostsAdapter()
+
 
   override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-      scores.add(10)
-      scores.add(20)
+
+
+
+      val db = Firebase.firestore
+
+/*
+      db.collection("products").get()
+          .addOnSuccessListener { queryDocumentSnapshots ->
+              progressBar.setVisibility(View.GONE)
+              if (!queryDocumentSnapshots.isEmpty) {
+                  val list = queryDocumentSnapshots.documents
+                  for (d in list) {
+                      val p: Product = d.toObject(Product::class.java)
+                      p.setId(d.id)
+                      productList.add(p)
+                  }
+                  adapter.notifyDataSetChanged()
+              }
+          }
+
+ */
+/*
+      db.collection("Scores")
+          //.whereEqualTo("user", FirebaseAuth.getInstance().currentUser.uid)
+          .orderBy("Score", Query.Direction.DESCENDING).limit(2)
+          .addSnapshotListener { value, e ->
+              if (e != null) {
+                  return@addSnapshotListener
+              }
+              //scoresList.clear()
+              val post = value.toObjects<Scores>()
+              for (scr in value!!.documents) {
+
+                  val post = Scores.fromHashMap(scr.data!!)
+
+                  scoresList.add(Scores(post.nome,post.score))
+                 // Log.e(value.toString(),post.nome.toString())
+              }
+
+          }
+
+
+ */
+      db.collection("Scores")
+          .orderBy("Score", Query.Direction.DESCENDING).limit(2)
+          .get()
+          .addOnSuccessListener { documents ->
+              for (document in documents) {
+                  val post = Scores.fromHashMap(document.data)
+
+
+                  val name = document.data?.let { it["Name"] as String? }
+                  val score = document.data?.let { it["Score"] as Long? }
+
+
+                   scoresList.add(Scores(name,score))
+              }
+
+              adapter.notifyDataSetChanged()
+          }
+          .addOnFailureListener { exception ->
+              Log.w(TAG, "Error getting documents: ", exception)
+          }
+
+
+
+      //scoresList.add(Scores("ok",10))
+/*
+      db.collection("Scores").orderBy("Score").limit(10).get()
+          .addOnSuccessListener { queryDocumentSnapshots ->
+              if (!queryDocumentSnapshots.isEmpty) {
+                  val list = queryDocumentSnapshots.documents
+                  for (d in list) {
+
+                          scoresList.add(d.toObject(Scores::class.java)!!)
+
+                  }
+
+              }
+          }
+
+
+ */
+
 
         _binding = FragmentHighscoreBinding.inflate(inflater, container, false)
         val root: View = binding.root
@@ -39,11 +137,11 @@ class  HighscoreFragment : Fragment() {
     }
     inner class PostsAdapter : BaseAdapter() {
         override fun getCount(): Int {
-            return scores.size
+            return scoresList.size
         }
 
         override fun getItem(position: Int): Any {
-            return scores[position]
+            return scoresList[position]
         }
 
         override fun getItemId(p0: Int): Long {
@@ -56,8 +154,8 @@ class  HighscoreFragment : Fragment() {
             val textViewScore = rootView.findViewById<TextView>(R.id.Score)
 
 
-            textviewId.text = (position+1).toString() + ": "
-            textViewScore.text = scores[position].toString() + " Points"
+            textviewId.text = scoresList[position].nome
+            textViewScore.text = scoresList[position].score.toString()
 
             return rootView
         }
@@ -67,10 +165,10 @@ class  HighscoreFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
         listviewScores = binding.listviewScores
-        val adapter = PostsAdapter()
+
         listviewScores.adapter = adapter
+
 
 
     }
--- app/src/main/java/com/example/projetocm/GameSystem/Scores.kt
+++ app/src/main/java/com/example/projetocm/GameSystem/Scores.kt
@@ -1,13 +1,39 @@
 package com.example.projetocm.GameSystem
 
+import com.google.firebase.Timestamp
+
 class Scores {
 
 
     var nome:String? = null
-    var score:Int? = null
+    var score:Long? = null
 
-    constructor(nome: String?, score: Int?) {
+    constructor()
+
+    constructor(nome: String?, score: Long?) {
         this.nome = nome
         this.score = score
+    }
+
+    fun toHashMap() : HashMap<String, Comparable<Any>?> {
+        val post = hashMapOf(
+            "Name" to nome,
+            "Score" to score,
+
+        )
+
+        return post as HashMap<String, Comparable<Any>?>
+    }
+
+    companion object {
+        fun fromHashMap(hash: Map<String, Any>) : Scores {
+            var post = Scores()
+
+            post.nome = hash["Name"] as String?
+            post.score = hash["Score"] as Long?
+
+
+            return post
+        }
     }
 }
