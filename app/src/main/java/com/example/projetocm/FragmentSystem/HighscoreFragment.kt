3a22d4d4049a26c6545bf987fa93a4ba23e7e476
Jorge <a21093@alunos.ipca.pt>
2022-01-16 17:55:14 +0000

    highscores

--- app/src/main/java/com/example/projetocm/FragmentSystem/HighscoreFragment.kt
+++ app/src/main/java/com/example/projetocm/FragmentSystem/HighscoreFragment.kt
@@ -1,35 +1,25 @@
 package com.example.projetocm.FragmentSystem
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
-import android.widget.ImageView
 import android.widget.ListView
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.findNavController
 import com.example.projetocm.R
 import com.example.projetocm.databinding.FragmentHighscoreBinding
-import com.google.firebase.Timestamp
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.firestore.SetOptions
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
-import java.util.*
-import java.util.jar.Attributes
 import kotlin.collections.ArrayList
 
 class  HighscoreFragment : Fragment() {
 
     private var _binding: FragmentHighscoreBinding? = null
     private val binding get() = _binding!!
-    private val names = ArrayList<Int>()
+    private val scores = ArrayList<Int>()
     lateinit var listviewScores: ListView
+    private val positions = ArrayList<Int>()
+    lateinit var listviewPositions: ListView
 
   override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -45,11 +35,11 @@ class  HighscoreFragment : Fragment() {
     }
     inner class PostsAdapter : BaseAdapter() {
         override fun getCount(): Int {
-            return names.size
+            return scores.size
         }
 
         override fun getItem(position: Int): Any {
-            return names[position]
+            return scores[position]
         }
 
         override fun getItemId(p0: Int): Long {
@@ -58,11 +48,12 @@ class  HighscoreFragment : Fragment() {
 
         override fun getView(position: Int, view: View?, viewGroup: ViewGroup?): View {
             val rootView = layoutInflater.inflate(R.layout.row_score, viewGroup, false)
-            val textviewTitle = rootView.findViewById<TextView>(R.id.Name)
+            val textviewTitle = rootView.findViewById<TextView>(R.id.Score)
             val textViewDescription = rootView.findViewById<TextView>(R.id.Score)
 
 
-            textviewTitle.text = names[position].toString()
+            textviewTitle.text = scores[position].toString()
+            textviewTitle.text = positions[position].toString()
 
             return rootView
         }
@@ -71,8 +62,7 @@ class  HighscoreFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        names.add(123)
-        names.add(144)
+
 
         listviewScores = binding.listviewScores
         val adapter = PostsAdapter()
